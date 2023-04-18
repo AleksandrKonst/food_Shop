92de93cdf626167f3d2f1d4aefdd65458575c840
AleksandrKonst <genkonst5@gmail.com>
2023-04-18 11:08:58 +0300

    first components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import {FormsModule} from "@angular/forms";
+import { CardComponent } from './Components/card/card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HeaderComponent, CardComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
