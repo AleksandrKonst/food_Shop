ae849370ee986d5a7d08a99cd178bb83bd6296dd
AleksandrKonst <genkonst5@gmail.com>
2023-04-18 11:58:48 +0300

    angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,13 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import {FormsModule} from "@angular/forms";
 import { CardComponent } from './Components/card/card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule, MatCardModule, MatButtonModule, MatToolbarModule],
   declarations: [AppComponent, HeaderComponent, CardComponent],
   bootstrap: [AppComponent]
 })
