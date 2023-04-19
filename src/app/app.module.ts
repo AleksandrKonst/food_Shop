65ac0b042fb3a00f653a486d41b6e87ba195e115
AleksandrKonst <genkonst5@gmail.com>
2023-04-19 20:28:05 +0300

    fix first tasks - add cards - add Log IN

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,22 @@ import { HeaderComponent } from './Components/header/header.component';
 import {FormsModule} from "@angular/forms";
 import { CardComponent } from './Components/card/card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {Routes, RouterModule} from '@angular/router';
+import { PanelCardsComponent } from './Components/panel-cards/panel-cards.component';
+import { LoginComponent } from './Components/login/login.component';
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
 
+const appRoutes: Routes =[
+  { path: '', component: PanelCardsComponent},
+  { path: 'login', component: LoginComponent},
+];
 @NgModule({
-  imports: [BrowserModule, FormsModule, BrowserAnimationsModule, MatCardModule, MatButtonModule, MatToolbarModule],
-  declarations: [AppComponent, HeaderComponent, CardComponent],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule, MatCardModule, MatButtonModule, MatToolbarModule, RouterModule.forRoot(appRoutes), MatInputModule, MatIconModule],
+  declarations: [AppComponent, HeaderComponent, CardComponent, PanelCardsComponent, LoginComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
